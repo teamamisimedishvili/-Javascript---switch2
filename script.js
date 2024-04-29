57830961f8b0bd643ad3d40c7ef53e2ca85ddb7e
teamamisimedishvili <127042618+teamamisimedishvili@users.noreply.github.com>
2024-04-29 14:37:52 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+var name1 = "dge";
+
+switch( name1 ) {
+
+    case "dila":
+
+        document.write("heloo");
+        break;
+
+        case "saramo":
+            document.write ("bye");
+            break;
+
+
+        default:
+            window.alert('"arasworia"')
+        
+
+}
